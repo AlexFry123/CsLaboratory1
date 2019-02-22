8f84bb4c6d55fbb17b4d1b029208479dad1427ba
AlexFry123 <45427927+AlexFry123@users.noreply.github.com>
2019-02-22 20:36:09 +0200

    Add files via upload

---
+++ CsLaboratory1/DateApplicationViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CsLaboratory1
+{
+    internal enum WesternZodiac { Ram, Bull, Twins, Crab, Lion, Virgin, Scales, Scorpion, Archer, Goat, WaterBearer, Fish };
+    internal enum ChineseZodiac { Monkey, Rooster, Dog, Pig, Rat, Ox, Tiger, Rabbit, Dragon, Snake, Horse, Goat };
+
+    class DateApplicationViewModel : INotifyPropertyChanged
+    {
+        private DateOfBirth _user;
+        private DateTime _chosenDate;
+        private string _age;
+        private string _zodiacWestern;
+        private string _zodiacChinese;
+        public Visibility LoaderVisibility { get; set; }
+        public bool IsEnabled { get; set; }
+
+        public DateTime ChosenDate
+        {
+            get { return _chosenDate; }
+            set
+            {
+                _chosenDate = value;
+                SettingDate();
+            }
+        }
+
+        public string Age
+        {
+            get { return "" + _user._age; }
+        }
+
+        public WesternZodiac ZodiacWestern
+        {
+            get { return _user._zodiacWestern; }
+        }
+
+        public ChineseZodiac ZodiacChinese
+        {
+            get { return _user._zodiacChinese; }
+        }
+
+        public DateApplicationViewModel()
+        {
+            LoaderVisibility = Visibility.Hidden;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("LoaderVisibility"));
+            _chosenDate = new DateTime(2000, 2, 12);
+            _user = new DateOfBirth(ref _chosenDate);
+            _age = "" + _user._age;
+            _zodiacWestern = "" + _user._zodiacWestern;
+            _zodiacChinese = "" + _user._zodiacChinese;
+            IsEnabled = true;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsEnabled"));
+        }
+
+        private async void SettingDate()
+        {
+            LoaderVisibility = Visibility.Visible;
+            IsEnabled = false;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsEnabled"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("LoaderVisibility"));
+            _user = new DateOfBirth(ref _chosenDate);
+            await Task.Run(() => Thread.Sleep(1500));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Age"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ZodiacWestern"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ZodiacChinese"));
+            IsEnabled = true;
+            LoaderVisibility = Visibility.Hidden;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsEnabled"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("LoaderVisibility"));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ CsLaboratory1/DateOfBirth.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Windows;
+
+namespace CsLaboratory1
+{
+    internal class DateOfBirth
+    {
+        public readonly int _age;
+        public readonly WesternZodiac _zodiacWestern;
+        public readonly ChineseZodiac _zodiacChinese;
+
+        public DateOfBirth(ref DateTime birthDate)
+        {
+            Birthday(ref birthDate);
+            try
+            {
+                _age = ChooseAge(ref birthDate);
+            } catch (ArgumentException argEx)
+            {
+                MessageBox.Show(argEx.Message);
+                birthDate = new DateTime(2000, 2, 12);
+                _age = ChooseAge(ref birthDate);
+            }
+            catch (ArithmeticException arithEx)
+            {
+                MessageBox.Show(arithEx.Message);
+                birthDate = new DateTime(2000, 2, 12);
+                _age = ChooseAge(ref birthDate);
+            }
+            finally
+            {
+                _zodiacWestern = ChooseSign(ref birthDate);
+                _zodiacChinese = CalcChineseZodiac(ref birthDate);
+            }
+        }
+
+        private void Birthday(ref DateTime dTime)
+        {
+            if (dTime.Day == DateTime.Today.Day && dTime.Month == DateTime.Today.Month)
+                MessageBox.Show("Happy Birthday!!!");
+        }
+
+        private int ChooseAge(ref DateTime dTime)
+        {
+            int age = DateTime.Today.Year - dTime.Year;
+            if (dTime > DateTime.Today.AddYears(-age)) age--;
+            if (age >= 135) throw new ArgumentException("You can't be so old!");
+            if (age < 0) throw new ArithmeticException("You haven't been born yet!");
+            return age;
+        }
+
+        private WesternZodiac ChooseSign(ref DateTime dTime)
+        {
+            if ((dTime.Day >= 21 && dTime.Month == 3) || (dTime.Day <= 20 && dTime.Month == 4))
+                return WesternZodiac.Ram;
+            else if ((dTime.Day >= 21 && dTime.Month == 4) || (dTime.Day <= 20 && dTime.Month == 5))
+                return WesternZodiac.Bull;
+            else if ((dTime.Day >= 21 && dTime.Month == 5) || (dTime.Day <= 20 && dTime.Month == 6))
+                return WesternZodiac.Twins;
+            else if ((dTime.Day >= 21 && dTime.Month == 6) || (dTime.Day <= 21 && dTime.Month == 7))
+                return WesternZodiac.Crab;
+            else if ((dTime.Day >= 22 && dTime.Month == 7) || (dTime.Day <= 21 && dTime.Month == 8))
+                return WesternZodiac.Lion;
+            else if ((dTime.Day >= 22 && dTime.Month == 8) || (dTime.Day <= 21 && dTime.Month == 9))
+                return WesternZodiac.Virgin;
+            else if ((dTime.Day >= 22 && dTime.Month == 9) || (dTime.Day <= 21 && dTime.Month == 10))
+                return WesternZodiac.Scales;
+            else if ((dTime.Day >= 22 && dTime.Month == 10) || (dTime.Day <= 21 && dTime.Month == 11))
+                return WesternZodiac.Scorpion;
+            else if ((dTime.Day >= 22 && dTime.Month == 11) || (dTime.Day <= 21 && dTime.Month == 12))
+                return WesternZodiac.Archer;
+            else if ((dTime.Day >= 22 && dTime.Month == 12) || (dTime.Day <= 20 && dTime.Month == 1))
+                return WesternZodiac.Goat;
+            else if ((dTime.Day >= 22 && dTime.Month == 1) || (dTime.Day <= 20 && dTime.Month == 2))
+                return WesternZodiac.WaterBearer;
+            else
+                return WesternZodiac.Fish;
+        }
+
+        private ChineseZodiac CalcChineseZodiac(ref DateTime birthDate)
+        {
+            return ((ChineseZodiac)(birthDate.Year % 12));
+        }
+    }
+}
