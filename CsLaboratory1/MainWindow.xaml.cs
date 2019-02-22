651353bb339d215458c9be3b3410018a74de838a
AlexFry123 <45427927+AlexFry123@users.noreply.github.com>
2019-02-22 22:55:52 +0200

    Update MainWindow.xaml.cs

--- CsLaboratory1/MainWindow.xaml.cs
+++ CsLaboratory1/MainWindow.xaml.cs
@@ -2,9 +2,6 @@ using System.Windows;
 
 namespace CsLaboratory1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
